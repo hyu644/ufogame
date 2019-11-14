eafc2bc97977ec37a6349f07b12b24be8f54393f
ikko <ikko@studio-ogawa.com>
2019-11-14 11:16:33 +0900

    Initial Commit

---
+++ UFOGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UFOGame
+//
+//  Created by cmStudent on 2019/11/14.
+//  Copyright © 2019 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
